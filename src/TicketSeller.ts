cd68e6ccb205c9dcb384c10174896171bae413a8
Hongbi <dyongdi@gmail.com>
2022-05-22 20:41:21 +0900

    오타 수정

--- src/TicketSeller.ts
+++ src/TicketSeller.ts
@@ -5,8 +5,8 @@ import TicketOffice from "./TicketOffice";
 export default class TicketSeller {
   private ticketOffice: TicketOffice;
 
-  constructor(ticektOffice: TicketOffice) {
-    this.ticketOffice = ticektOffice;
+  constructor(ticketOffice: TicketOffice) {
+    this.ticketOffice = ticketOffice;
   }
 
   sellTo(audience: Audience) {
