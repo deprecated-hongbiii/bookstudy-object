e003521d4e5716dd386ab135f22a3e5ece7c8195
Hongbi <dyongdi@gmail.com>
2022-05-22 20:39:32 +0900

    [1-3] TicketSeller 클래스 캡슐화
    - Theater 클래스의 enter 메서드에서 하던 일을 TicketSeller가 주체적으로 하도록 캡슐화
    - TicketSeller에서 getTicketOffice 메서드 제거. 외부에서 ticketOffice에 직접 접근하지 못하도록 했다.
    - 수정된 Theater는 ticketOffice가 TicketSeller 내부에 존재한다는 사실을 알지 못한다.
    - 단지 ticketSeller가 sellTo 메시지를 이해하고 응답할 수 있다는 사실만 알고 있다.
    - 이제 Theater는 오직 TicketSeller의 인터페이스에만 의존한다.
    - TicketSeller가 내부에 TicketOffice 인스턴스를 포함하고 있다는 사실은 구현의 영역이다.

--- src/Theater.ts
+++ src/Theater.ts
@@ -1,5 +1,4 @@
 import Audience from "./Audience";
-import Ticket from "./Ticket";
 import TicketSeller from "./TicketSeller";
 
 export default class Theater {
@@ -10,14 +9,6 @@ export default class Theater {
   }
 
   enter(audience: Audience) {
-    if(audience.getBag().hasInvitation()) {
-      const ticket: Ticket = this.ticketSeller.getTicketOffice().getTicket()!; // undefined 타입에러 없애기 위해 사용
-      audience.getBag().setTicket(ticket);
-    } else {
-      const ticket: Ticket = this.ticketSeller.getTicketOffice().getTicket()!; // undefined 타입에러 없애기 위해 사용
-      audience.getBag().minusAmount(ticket.getFee());
-      this.ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-      audience.getBag().setTicket(ticket);
-    }
+    this.ticketSeller.sellTo(audience);
   }
 }
--- src/TicketSeller.ts
+++ src/TicketSeller.ts
@@ -1,3 +1,5 @@
+import Audience from "./Audience";
+import Ticket from "./Ticket";
 import TicketOffice from "./TicketOffice";
 
 export default class TicketSeller {
@@ -7,7 +9,15 @@ export default class TicketSeller {
     this.ticketOffice = ticektOffice;
   }
 
-  getTicketOffice() {
-    return this.ticketOffice;
+  sellTo(audience: Audience) {
+    if(audience.getBag().hasInvitation()) {
+      const ticket: Ticket = this.ticketOffice.getTicket()!; // undefined 타입에러 없애기 위해 사용
+      audience.getBag().setTicket(ticket);
+    } else {
+      const ticket: Ticket = this.ticketOffice.getTicket()!; // undefined 타입에러 없애기 위해 사용
+      audience.getBag().minusAmount(ticket.getFee());
+      this.ticketOffice.plusAmount(ticket.getFee());
+      audience.getBag().setTicket(ticket);
+    }
   }
 }
