34186b78eef5dd0462e0602a4cc71aebba14dc01
Hongbi <dyongdi@gmail.com>
2022-06-06 23:24:07 +0900

    [2-3] Movie는 단지 discountPolicy에게 메시지를 전송할 뿐이다.
    - calculateMovieFee 메서드 안에는 한 가지 이상한 점이 있다.
    - 어떤 할인 정책을 사용할 것인지 결정하는 코드가 어디에도 존재하지 않는다.
    - 단지 discountPolicy에게 메시지를 전송할 뿐이다.
    - 이 코드에는 상속과 다형성의 개념이 숨겨져 있다.
    - 그리고 그 기반에는 추상화라는 원리가 숨겨져 있다.

--- src/Movie.ts
+++ src/Movie.ts
@@ -1,3 +1,24 @@
+import Money from "./Money";
+import Screening from "./Screening";
+
 export default class Movie {
-  getFee() {}
+  private title: string;
+  private runningTime: number; // 원본 자바코드에서는 Duration 타입
+  private fee: Money;
+  private discountPolicy: DiscountPolicy;
+
+  constructor(title: string, runningTime: number, fee: Money, discountPolicy: DiscountPolicy) {
+    this.title = title;
+    this.runningTime = runningTime;
+    this.fee = fee;
+    this.discountPolicy = discountPolicy;
+  }
+
+  getFee() {
+    return this.fee;
+  }
+
+  calculateMovieFee(screening: Screening): Money {
+    return this.fee.minus(this.discountPolicy.calculateDiscountAmount(screening));
+  }
 }
